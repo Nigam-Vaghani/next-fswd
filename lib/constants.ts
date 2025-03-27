e3ca553c59dab70e02064748b4e30b862e22b273
abc <you@example.com>
2025-03-27 12:42:25 +0530

    02

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'JSS'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, ENJOY more'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'an JSS is build by NIRMA studet with NEXT and MONGO'
