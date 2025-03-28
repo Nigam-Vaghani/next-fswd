8902bbf4153a6528501eb527df95c3e4d2bf1b32
abc <you@example.com>
2025-03-28 23:20:02 +0530

    03

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,5 @@
+// import { title } from 'process'
+
 const data = {
   headerMenus: [
     {
@@ -31,6 +33,29 @@ const data = {
     {
       name: 'Help',
       href: '/page/help',
+    },
+  ],
+  carousles: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
     },
   ],
 }
